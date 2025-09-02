146dbf457c3f2282949fe1f63f3e239a4a67e527
rebecalazarini <rebecalazarini2007@gmail.com>
2025-09-02 15:25:46 -0300

    corrigindo erros

--- api/api/server.js
+++ api/api/server.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+const routes = require("../src/routes");
+
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
